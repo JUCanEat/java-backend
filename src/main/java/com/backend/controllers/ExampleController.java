1a6e14a8857f0c6be6040e4222674c758814ca3c
Hania <haniazipser2004@gmail.com>
2025-09-20 14:17:54 +0200

    feat: add dev database

--- src/main/java/com/backend/controllers/ExampleController.java
+++ src/main/java/com/backend/controllers/ExampleController.java
@@ -1,16 +1,30 @@
 package com.backend.controllers;
 
+import com.backend.model.ExampleClass;
+import com.backend.services.ExampleService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * Example controller
  */
 @RestController
 @RequestMapping("/hello")
+@RequiredArgsConstructor
 public class ExampleController {
+    private final ExampleService exampleService;
     @GetMapping("/")
     public String helloWorld(){
         return "Hello world";
+    }
+
+    @GetMapping("/example")
+    public List<ExampleClass> exampleEndpoint(){
+        return exampleService.getAll();
     }
 }
