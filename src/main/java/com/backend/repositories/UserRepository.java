5fa84ed5b1b74c684a80de05329a0eb5eec51c76
kingazm <kinga.zmuda@icloud.com>
2025-10-23 15:54:38 +0200

    Add missing JpaRepositories

--- src/main/java/com/backend/repositories/UserRepository.java
+++ src/main/java/com/backend/repositories/UserRepository.java
@@ -1,2 +1,11 @@
-package com.backend.repositories;public interface UserRepository {
+package com.backend.repositories;
+
+import com.backend.model.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
