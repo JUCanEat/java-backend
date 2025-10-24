26314ac4a9131e29785a60f469fe3e26c74701df
Hania <haniazipser2004@gmail.com>
2025-10-24 14:39:20 +0200

    feat: implement Location with value objects

---
+++ src/main/java/com/backend/model/valueObjects/Longitude.java
@@ -0,0 +1,20 @@
+package com.backend.model.valueObjects;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Embeddable
+public class Longitude {
+
+    private Double value;
+
+    public Longitude(Double value) {
+        if (value == null || value < -180 || value > 180) {
+            throw new IllegalArgumentException("Longitude must be between -180 and 180");
+        }
+        this.value = value;
+    }
+}
