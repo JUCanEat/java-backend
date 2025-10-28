ad89e128861a60c4ce3b03077c63b05bdd27e593
Hania <haniazipser2004@gmail.com>
2025-10-28 17:40:46 +0100

    feat: add keycloak integration with RBAC

---
+++ src/main/java/com/backend/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.backend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+        @Bean
+        SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+            JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+            jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+            http.cors(Customizer.withDefaults()).csrf(c -> c.disable())
+                    .authorizeHttpRequests((a) -> a
+                                    //.requestMatchers(HttpMethod.POST, "/api/restaurants").hasRole("restaurant_owner")
+                                    //.anyRequest().authenticated()
+                                    .anyRequest().permitAll());
+            http.oauth2ResourceServer(rsc -> rsc.jwt(jwtConfigurer ->
+                    jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+            return http.build();
+        }
+
+}
--- src/main/java/com/backend/controllers/RestaurantController.java
+++ src/main/java/com/backend/controllers/RestaurantController.java
@@ -9,11 +9,16 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
@@ -54,5 +59,24 @@ public class RestaurantController {
 
         RestaurantDetailsDTO restaurant = restaurantService.getRestaurantById(id);
         return ResponseEntity.ok(restaurant);
+    }
+
+    @Operation(
+            summary = "Add daily menu for restaurant by ID",
+            description = "Retrieves detailed information about a specific restaurant, including location, opening hours, and today's menu."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved restaurant details"),
+            @ApiResponse(responseCode = "404", description = "Restaurant not found"),
+            @ApiResponse(responseCode = "403", description = "User is not the owner of the restaurant"),
+    })
+
+    @PostMapping("/{id}")
+    @PreAuthorize("hasRole('restaurant_owner')")
+    public ResponseEntity<RestaurantDetailsDTO> AddDailyMenu(
+            @Parameter(description = "Id of the restaurant", required = true)
+            @PathVariable UUID id) {
+
+        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
     }
 }
