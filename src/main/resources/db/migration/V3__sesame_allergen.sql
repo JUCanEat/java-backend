32181f386013f01787fe3013409ed8ecffdf5faa
Hania <haniazipser2004@gmail.com>
2026-05-14 21:23:42 +0200

    fix: resolve conflict

--- src/main/resources/db/migration/V3__sesame_allergen.sql
+++
@@ -1,6 +0,0 @@
-LTER TABLE dish_allergens
-    DROP CONSTRAINT IF EXISTS dish_allergens_allergen_check;
-
-ALTER TABLE dish_allergens
-    ADD CONSTRAINT dish_allergens_allergen_check
-    CHECK (allergen IN ('NUTS', 'GLUTEN', 'MEAT', 'LACTOSE', 'SESAME'));
